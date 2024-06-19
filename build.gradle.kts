d93a5a11f60e09a7d63511c9ef8e1f615b19fe12
William Okano <williamokano@gmail.com>
2024-06-19 21:38:30 +0200

    feat: added task to run arbitrary code from the examples folder

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,15 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.register<JavaExec>("processLoxFile") {
+    // Set the main class to be executed
+    mainClass.set("okano.dev.lox.Lox")
+
+    // Configure the classpath to include the project's runtime classpath
+    classpath = sourceSets["main"].runtimeClasspath
+
+    // Pass the file argument to the main class
+    val inputFile = project.findProperty("file") as String? ?: ""
+    args = listOf(inputFile)
+}
